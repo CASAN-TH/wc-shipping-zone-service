d1e6a8d51a02ed10cea8e99d2660f24b0cb7455b
jigkoh3@hotmail.com <porapot2@gmail.com>
2019-12-04 12:11:18 +0700

    first commit

---
+++ src/modules/shippingzone/models/model.js
@@ -0,0 +1,53 @@
+'use strict';
+// use model
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var ShippingzoneSchema = new Schema({
+    // name: {
+    //     type: String,
+    //     required: 'Please fill a Shippingzone name',
+    // },
+
+    name: {
+        type: String,
+        
+    },
+    order: {
+        type: Number,
+        
+    },
+
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    updated: {
+        type: Date
+    },
+    createby: {
+        _id: {
+            type: String
+        },
+        username: {
+            type: String
+        },
+        displayname: {
+            type: String
+        }
+    },
+    updateby: {
+        _id: {
+            type: String
+        },
+        username: {
+            type: String
+        },
+        displayname: {
+            type: String
+        }
+    }
+});
+
+mongoose.model("Shippingzone", ShippingzoneSchema);
---
+++ src/modules/shippingzone/test/routes.js
@@ -0,0 +1,234 @@
+'use strict';
+var request = require('supertest'),
+    assert = require('assert'),
+    config = require('../../../config/config'),
+    _ = require('lodash'),
+    jwt = require('jsonwebtoken'),
+    mongoose = require('mongoose'),
+    app = require('../../../config/express'),
+    Shippingzone = mongoose.model('Shippingzone');
+
+var credentials,
+    token,
+    mockup;
+
+describe('Shippingzone CRUD routes tests', function () {
+
+    before(function (done) {
+        mockup = {
+            name: "Brazil",
+            order: 0
+        };
+        credentials = {
+            username: 'username',
+            password: 'password',
+            firstname: 'first name',
+            lastname: 'last name',
+            email: 'test@email.com',
+            roles: ['user']
+        };
+        token = jwt.sign(_.omit(credentials, 'password'), config.jwt.secret, {
+            expiresIn: 2 * 60 * 60 * 1000
+        });
+        done();
+    });
+
+    it('should be Shippingzone get use token', (done) => {
+        request(app)
+            .get('/api/shippingzones')
+            .set('Authorization', 'Bearer ' + token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                done();
+            });
+    });
+
+    it('should be Shippingzone get by id', function (done) {
+
+        request(app)
+            .post('/api/shippingzones')
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                request(app)
+                    .get('/api/shippingzones/' + resp.data._id)
+                    .set('Authorization', 'Bearer ' + token)
+                    .expect(200)
+                    .end(function (err, res) {
+                        if (err) {
+                            return done(err);
+                        }
+                        var resp = res.body;
+                        assert.equal(resp.status, 200);
+                        assert.equal(resp.data.name, mockup.name);
+                        assert.equal(resp.data.order, mockup.order);
+
+                        done();
+                    });
+            });
+
+    });
+
+    it('should be Shippingzone post use token', (done) => {
+        request(app)
+            .post('/api/shippingzones')
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                assert.equal(resp.data.name, mockup.name);
+                assert.equal(resp.data.order, mockup.order);
+                done();
+            });
+    });
+
+    it('should be shippingzone put use token', function (done) {
+
+        request(app)
+            .post('/api/shippingzones')
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                var update = {
+                    name: "Brazil1",
+                    order: 1
+                }
+                request(app)
+                    .put('/api/shippingzones/' + resp.data._id)
+                    .set('Authorization', 'Bearer ' + token)
+                    .send(update)
+                    .expect(200)
+                    .end(function (err, res) {
+                        if (err) {
+                            return done(err);
+                        }
+                        var resp = res.body;
+                        assert.equal(resp.data.name, update.name);
+                        assert.equal(resp.data.order, update.order);
+                        done();
+                    });
+            });
+
+    });
+
+    it('should be shippingzone delete use token', function (done) {
+
+        request(app)
+            .post('/api/shippingzones')
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                request(app)
+                    .delete('/api/shippingzones/' + resp.data._id)
+                    .set('Authorization', 'Bearer ' + token)
+                    .expect(200)
+                    .end(done);
+            });
+
+    });
+
+    it('should be shippingzone get not use token', (done) => {
+        request(app)
+            .get('/api/shippingzones')
+            .expect(403)
+            .expect({
+                status: 403,
+                message: 'User is not authorized'
+            })
+            .end(done);
+    });
+
+    it('should be shippingzone post not use token', function (done) {
+
+        request(app)
+            .post('/api/shippingzones')
+            .send(mockup)
+            .expect(403)
+            .expect({
+                status: 403,
+                message: 'User is not authorized'
+            })
+            .end(done);
+
+    });
+
+    it('should be shippingzone put not use token', function (done) {
+
+        request(app)
+            .post('/api/shippingzones')
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                var update = {
+                    name: 'name update'
+                }
+                request(app)
+                    .put('/api/shippingzones/' + resp.data._id)
+                    .send(update)
+                    .expect(403)
+                    .expect({
+                        status: 403,
+                        message: 'User is not authorized'
+                    })
+                    .end(done);
+            });
+
+    });
+
+    it('should be shippingzone delete not use token', function (done) {
+
+        request(app)
+            .post('/api/shippingzones')
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                request(app)
+                    .delete('/api/shippingzones/' + resp.data._id)
+                    .expect(403)
+                    .expect({
+                        status: 403,
+                        message: 'User is not authorized'
+                    })
+                    .end(done);
+            });
+
+    });
+
+    afterEach(function (done) {
+        Shippingzone.remove().exec(done);
+    });
+
+});
